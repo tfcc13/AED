37c78361c61fbc93f9a7241145ea9d5956c444d7
Tiago <tiagofccarvalho13@gmail.com>
2023-10-20 14:10:31 +0100

    Made exercise 2

--- P04/Tests/funSetProblem.cpp
+++ P04/Tests/funSetProblem.cpp
@@ -8,5 +8,14 @@ FunSetProblem::FunSetProblem() {}
 //=============================================================================
 // TODO
 pair<int,int> FunSetProblem::pairSum(const vector<int> &values, int sum) {
+    set<int> valSet;
+    for (int val : values) {
+        int comp = sum - val;
+
+        if (valSet.find(comp) != valSet.end()){
+            return make_pair(val, comp);
+        }
+        valSet.insert(val);
+    }
     return make_pair(0,0);
 }
