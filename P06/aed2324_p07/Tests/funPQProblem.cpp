e218ce80fc18b4387c7e0c0045bbb2f6aeea1c84
Tiago <tiagofccarvalho13@gmail.com>
2023-11-08 17:33:27 +0000

    Made P06 ex 2

--- P06/aed2324_p07/Tests/funPQProblem.cpp
+++ P06/aed2324_p07/Tests/funPQProblem.cpp
@@ -8,5 +8,25 @@ FunPQProblem::FunPQProblem() {}
 //=============================================================================
 // TODO
 int FunPQProblem::minCost(const vector<int> &ropes) {
-    return 0;
+    int cost = 0;
+    int tempRope = 0;
+
+    priority_queue<int, std::vector<int>, std::greater<int>> ropesToConnect;
+
+    for ( const int rope : ropes) {
+        ropesToConnect.push(rope);
+    }
+
+    while(ropesToConnect.size() > 1) {
+        tempRope = 0;
+        tempRope += ropesToConnect.top();
+        ropesToConnect.pop();
+        tempRope += ropesToConnect.top();
+        ropesToConnect.pop();
+        cost += tempRope;
+        ropesToConnect.push(tempRope);
+    }
+
+
+    return cost;
 }
